dac464a7a5ce14a0972c0a001362bd77e1f6430d
Crowdbotics <team@crowdbotics.com>
2019-06-20 09:18:41 +0000

    initial commit

---
+++ android/app/src/main/java/com/mgate_loans_4923/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mgate_loans_4923;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "mgate_loans_4923";
+    }
+}
